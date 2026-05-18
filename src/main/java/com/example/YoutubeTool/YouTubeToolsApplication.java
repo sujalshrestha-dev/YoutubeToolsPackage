2e2870d824c305fce65ca8efe89969b16b86cfea
Sujal Shrestha <shresthasujal2021@gmail.com>
2026-05-18 18:49:46 +0545

    Initial commit

---
+++ src/main/java/com/example/YoutubeTool/YouTubeToolsApplication.java
@@ -0,0 +1,13 @@
+package com.example.YoutubeTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouTubeToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouTubeToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/YoutubeTool/YoutubeToolsPackageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.YoutubeTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeToolsPackageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
